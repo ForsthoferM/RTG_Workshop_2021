8f6e2ae218bc8a89f9b06bd045c35b6843aa901f
Michi <59614807+FrogSquad5000@users.noreply.github.com>
2021-10-19 10:32:09 +0200

    added serialization and read pwdb

--- auth.py
+++ auth.py
@@ -5,6 +5,7 @@ Created on Tue Oct 19 09:27:14 2021
 @author: mfors
 """
 
+import json
 
 def get_credentials():
         username = input('Type username: ')
@@ -22,9 +23,19 @@ def authenticate(username, password, pwdb):
     else:
         print('User not known!')
     return auth
- 
-pwdb = {'tiziano' : 'abc123', 
-        'pamela' : 'qwerty',
-        'michi' : 'test123'}
+
+def write_pwdb(pwdb):
+    with open('pwdb.json', 'wt') as pwdb_file:
+        json.dump(pwdb, pwdb_file)
+    print('Pwdb written!')
+# with: context, this will open the file and close it afterwards. 'wt' is for write text
+    
+def read_pwdb(pwdb_file):
+    with open('pwdb.json', 'rt') as pwdb_file:
+        pwdb = json.load(pwdb_file)
+        return pwdb
+# 'rt' for read text
+
 username, password = get_credentials()
+pwdb = read_pwdb()
 authenticate(username, password, pwdb)
